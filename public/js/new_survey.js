ff0118e10fe3d35f95177740f20bafd016c76c9d
leahgoldberg <leahgoldberg31@gmail.com>
2015-08-30 18:34:39 -0400

    refactor ajax calls

--- public/js/new_survey.js
+++ public/js/new_survey.js
@@ -1,27 +1,29 @@
 $('document').ready(function(){
   $("#new-question-form").on("submit", "#add-question",function(e){
     e.preventDefault();
-    $.ajax('/questions', {
-      data: $(this).serialize(),
-      method: 'POST'
-    }).done(function(data){
-      // $('#add-choice-div').show();
-      $('#add-choice-div').append(data);
-      // debugger
+    $this = $(this);
+    $.ajax({
+      url: $this.attr('action'),
+      data: $this.serialize(),
+      method: $this.attr('method')
+    }).done(function(response){
+      $('#add-choice-div').append(response);
+    }).fail(function() {
+      console.log("Didn't add new choice correctly")
     });
 
    $('#add-choice-div').on('submit','#new-choice', function(e){
       e.preventDefault();
       $this = $(this);
-      $.ajax('/choices', {
+      $.ajax({
+        url: $this.attr('action'),
         data: $this.serialize(),
-        method: 'POST'
+        method: $this.attr('method')
       }).done(function(response){
         $('#new-choice').remove();
         $('#add-question').replaceWith(response);
-        debugger
       }).fail(function() {
-        debugger
+        console.log("Didn't add choice correctly")
       });
 
       });
