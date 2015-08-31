9f037a255716e0b1dbec0aacbc82c5ab8750df8d
leahgoldberg <leahgoldberg31@gmail.com>
2015-08-31 01:28:44 -0400

    style progress bar

--- public/js/application.js
+++ public/js/application.js
@@ -17,13 +17,11 @@ $('document').ready(function(){
   var stats = $('.stats-bar')
 
   for (var i=0; i<stats.length; i++) {
-    debugger
-    $(stats[i]).goalProgress(
-    {
+    $(stats[i]).goalProgress({
     goalAmount: 100,
     currentAmount: parseInt($(stats[i]).text()),
-    textBefore: '$',
-    textAfter: ' raised'
+    textBefore: '',
+    textAfter: '%'
     });
   }  
 
